7795c57d813e89cf9ae67a89c0c417029ee27b01
Ying Wang <ywang.botany@gmail.com>
2016-10-10 01:40:42 -0400

    templateUrl in items.component.js was wrong

--- module4-solution/src/items.component.js
+++ module4-solution/src/items.component.js
@@ -3,7 +3,7 @@
 
 	angular.module('MenuApp')
 	.component('items',{
-		templateUrl: '/src/templates/items.template.html',
+		templateUrl: 'src/templates/items.template.html',
   		// controller: ShoppingListComponentController,
 		bindings: {
 		items: '<'
